3e008a00e55f1954910fa7d47c3572091e822b7b
Duy Truong <ted.truongbuuduy@gmail.com>
2024-09-09 23:12:24 +0700

    UI SignIn

--- src/components/signIn.tsx
+++ src/components/signIn.tsx
@@ -1,13 +1,11 @@
 import React, { ChangeEvent, FormEvent, useState } from "react";
 
+import "./login.scss";
 import { UserType } from "../app/types";
-import "../styles/_login.scss";
-import { useDispatch } from "react-redux";
 import { useCreateUserMutation } from "../features/user/userApi";
-import { use } from "framer-motion/client";
+import CatDog from "../../public/Confused Dog & Cat.jpeg";
 
 const SignIn = () => {
-  const dispatch = useDispatch();
   const [createUser, { isLoading, isError, isSuccess }] =
     useCreateUserMutation();
 
@@ -16,7 +14,7 @@ const SignIn = () => {
     lastname: "",
     username: "",
     password: "",
-    repeatPassword: '',
+    repeatPassword: "",
     email: "",
     phone: "",
   });
@@ -38,83 +36,89 @@ const SignIn = () => {
     };
 
     try {
-      // Trigger the createUser mutation
       await createUser(UserForm).unwrap();
-      alert("User registered successfully!");
-      console.log(UserForm)
+      console.log(UserForm);
     } catch (error) {
       console.error("Failed to register user: ", error);
-      alert("User registration failed!");
     }
   };
 
   return (
     <div className="container">
-      <form className="" onSubmit={handleSubmit}>
-        <div className="">
+      <div className="form-section">
+        <h1>Sign In</h1>
+        {isError && <p>Error registering user.</p>}
+        {isSuccess && <p>User registered successfully!</p>}
+        <form className="form-container" onSubmit={handleSubmit}>
+          <div>
+            <input
+              type="text"
+              name="firstname"
+              placeholder="First name"
+              value={user.firstname}
+              onChange={handleChange}
+              className="input-1"
+              required
+            />
+            <input
+              type="text"
+              name="lastname"
+              placeholder="Last name"
+              value={user.lastname}
+              onChange={handleChange}
+              required
+            />
+          </div>
           <input
             type="text"
-            name="firstname"
-            placeholder="First name"
-            value={user.firstname}
+            name="username"
+            placeholder="User name"
+            value={user.username}
+            onChange={handleChange}
+            required
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={user.password}
+            onChange={handleChange}
+            required
+          />
+          <input
+            type="password"
+            name="repeatPassword"
+            placeholder="Repeat your password"
+            value={user.repeatPassword}
+            onChange={handleChange}
+            required
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={user.email}
             onChange={handleChange}
             required
           />
           <input
             type="text"
-            name="lastname"
-            placeholder="Last name"
-            value={user.lastname}
+            name="phone"
+            placeholder="Phone"
+            value={user.phone}
             onChange={handleChange}
             required
           />
-        </div>
-        <input
-          type="text"
-          name="username"
-          placeholder="User name"
-          value={user.username}
-          onChange={handleChange}
-          required
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          value={user.password}
-          onChange={handleChange}
-          required
-        />
-        <input
-          type="password"
-          name="repeatpassword"
-          placeholder="Repeat your password"
-          value={user.repeatPassword}
-          onChange={handleChange}
-          required
-        />
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          value={user.email}
-          onChange={handleChange}
-          required
-        />
-        <input
-          type="text"
-          name="phone"
-          placeholder="Phone"
-          value={user.phone}
-          onChange={handleChange}
-          required
-        />
-        <button type="submit" disabled={isLoading}>
-          {isLoading ? "Registering..." : "Register"}
-        </button>
-      </form>
-      {isError && <p>Error registering user.</p>}
-      {isSuccess && <p>User registered successfully!</p>}
+          <button type="submit" disabled={isLoading}>
+            {isLoading ? "Registering..." : "Register"}
+          </button>
+        </form>
+        
+      </div>
+      <figure>
+        <img src={CatDog} />
+        <a>Already have account?</a>
+      </figure>
     </div>
   );
 };
